e3b296d2b43b8cd2884ac8ecc635c43463254a81
Idan Badin <Idanb@nayax.com>
2023-09-04 19:36:34 +0300

    First Commit

---
+++ index.js
@@ -0,0 +1,430 @@
+
+receipt = {
+  "CustomerName": "עידן בדין",
+  "ID": "1ad938d9-0848-40e5-9047-e731e66b9aa7",
+  "OriginalText": null,
+  "Total": 140,
+  "TotalNoVat": 119.66,
+  "VatTotal": 20.34,
+  "Vat": 0,
+  "Currency": "ש\"ח",
+  "Discount": "0.00",
+  "Barcode": null,
+  "TransactionNumber": "91000002",
+  "PaymentTypeStr": "Credit",
+  "PaymentType": 1,
+  "ReceiptType": 1,
+  "UploadedDate": null,
+  "CreatedDate": "2023-08-30T08:17:59.8Z",
+  "Action": 1,
+  "Target": "0544532949",
+  "AdditionalTargets": [],
+  "POSId": null,
+  "BusinessID": "4fc9ed41-e37a-4630-9689-9c64c33248eb",
+  "BranchID": null,
+  "POSInternalId": null,
+  "tPOS": {
+    "InternalID": "3dc0611a-1ded-4eae-a8e2-5f1f14b700a8.1",
+    "Description": "1",
+    "IsOnline": false
+  },
+  "tBranch": {
+    "InternalID": "1",
+    "Latitude": 32.1605093,
+    "Longitude": 34.8092981,
+    "Language": 2,
+    "CompanyID": null,
+    "BranchName": "The Table Fairy",
+    "BranchAddress": "אריה שנקר 4,  הרצליה",
+    "BranchAddressEng": "Arye Shenkar St 4, Herzliya, Israel",
+    "BranchPhone": "052-2535195",
+    "Description": "פיית השולחן",
+    "IsOnline": false,
+    "FranchiseeName": null,
+    "VatNumber": "558475034",
+    "SAPId": null
+  },
+  "tBusiness": null,
+  "IsFavorite": false,
+  "ExtraDiscreteField1": null,
+  "ExtraDiscreteField2": null,
+  "ExtraTextField1": null,
+  "ExtraTextField2": null,
+  "LoyaltyID": null,
+  "LoyalName": null,
+  "CashierName": "מאיה צח",
+  "Items": [
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "25.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "25.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "25.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "0.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "0.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "0.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "0.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "0.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+    {
+      "Name": "Starburst Napkin Ring in White",
+      "Discount": null,
+      "Position": null,
+      "ItemCode": "13221",
+      "Price": 140,
+      "Quantity": 1,
+      "DiscountCalc": 0,
+      "Total": 140,
+      "SubItems": [],
+      "ParentId": null,
+      "AdditionalData": [
+        {
+          "Key": "BarCode",
+          "Value": "2000000004426"
+        },
+        {
+          "Key": "Discount",
+          "Value": "0.00"
+        },
+        {
+          "Key": "PriceAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "AmountAfterDiscount",
+          "Value": "140.00"
+        },
+        {
+          "Key": "discountPercent",
+          "Value": "0.00"
+        }
+      ],
+      "ItemInfo": null,
+      "Categories": []
+    },
+  ],
+  "Payments": [
+    {
+      "Name": "מסטרכרט",
+      "PaymentCode": "28",
+      "Amount": 1680,
+      "PaymentInfo": "\nTotalPayments: 1.00\nFirstPaymentAmount: 0.00\nCreditCardLast4Dig: 555578***0534",
+      "LastDigits": 2345,
+      "UID": 96841564981268974894988974984,
+      "RRN": null,
+      "AdditionalData": null
+    }
+  ],
+  "MoneySaved": null,
+  "ItemsCount": 1,
+  "OrderID": null,
+  "SuppressSMS": false
+}
+
+function formatNumber(number) {
+  const formattedNumber = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'ILS' }).format(number);
+  return formattedNumber;
+}
+
+function getAddtionalValue(item, Key) {
+  return item.AdditionalData && item.AdditionalData.find(a=>a.Key == Key) ? item.AdditionalData.find(a=>a.Key == Key).Value : "bve";
+}
+
+$(".businessName").text(receipt.tBranch.BranchName);
+$(".businessAddress").text(receipt.tBranch.BranchAddress);
+$(".businessPhone").text(receipt.tBranch.BranchPhone);
+
+var paymentMethod = receipt.Payments[0].Name === "מסטרכרט" ? "אשראי" : receipt.Payments.Name;
+$(".customerName").html("<p style='font-weight: bold;width: fit-content'>שם לקוח: "+receipt.CustomerName+"</p>")
+
+var total = 0;
+for(var i=0; i < receipt.Items.length; i++) {
+  var item = receipt.Items[i];
+  var itemCode = getAddtionalValue(item, "BarCode");
+  var itemDiscount = getAddtionalValue(item, "discountPercent");
+  $("#itemsTbl").append("<tr class='spaceUnder' style='font-size: 14px'><td id='itemNameCell'>"+item.Name+"</td><td>"+item.Quantity+"</td><td>"+itemDiscount+"%</td><td style='padding-right: 10px'>"+formatNumber(item.Total)+"</td></tr>");
+  $("#itemsTbl").append("<tr class='itemCodeCell' style='font-size: 10px'><td colspan='4'>  קוד פריט: "+itemCode+"</td></tr>");
+  $("#itemsTbl").append("<tr class='hr'><td colspan='4'></td></tr>");
+  if(item.Name.length >= 25) {
+    $("#itemNameCell").css({"width": "180px"});
+  }
+  total += item.Total;
+}
+$("#itemsTable").append("<tr><td style='padding: 10px 0 0 0' colspan='3'>סה\"כ לתשלום</td><td style='padding: 10px 0 0 0' colspan='1'>"+formatNumber(total)+"</td><div>");
+$("#itemsTable").append("<tr><td style='padding: 0' colspan='3'>אופן תשלום</td><td style='padding: 0' colspan='1'>"+paymentMethod+"</td><div>");
